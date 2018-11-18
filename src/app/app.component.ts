158a3068ca646158280e2003bd8cb87b585acf58
rafadpedrosa <rafadpedrosa@gmail.com>
2018-11-18 15:32:01 -0200

    Add dashboard to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'coffe-time-group-web';
+  coffeHistory$: Observable<any>;
+
+  constructor(private db: AngularFirestore) {
+  }
+
+  ngOnInit() {
+    this.getCoffeHistory();
+  }
+
+  getCoffeHistory() {
+    this.coffeHistory$ = this.db
+    .collection('coffeHistory')
+    .snapshotChanges()
+    .pipe(
+      map(response => response.map(doc => doc.payload.doc.data()))
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StatusIndicatorBarComponent } from './dashboard/status-indicator-bar/status-indicator-bar.component';
+import { CoffeeService } from './services/coffee.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    StatusIndicatorBarComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [ CoffeeService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
