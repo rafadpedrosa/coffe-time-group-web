1c417e82ab66edad18838f77601db127504c44b7
rafadpedrosa <rafadpedrosa@gmail.com>
2018-11-21 21:25:46 -0200

    #3 Adding messaging service

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,94 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireMessaging } from '@angular/fire/messaging';
+import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  currentMessage = new BehaviorSubject(null);
+  token: string;
+
+  constructor(
+    private angularFireDB: AngularFirestore,
+    private angularFireAuth: AngularFireAuth,
+    private angularFireMessaging: AngularFireMessaging,
+    private http: HttpClient,
+  ) {
+    this.angularFireMessaging.messaging.subscribe(
+      (_messaging) => {
+        _messaging.onMessage = _messaging.onMessage.bind(_messaging);
+        _messaging.onTokenRefresh = _messaging.onTokenRefresh.bind(_messaging);
+      }
+    );
+  }
+
+  /**
+   * update token in firebase database
+   *
+   * @param userId userId as a key
+   * @param token token as a value
+   */
+  updateToken(userId, token) {
+    // we can change this function to request our backend service
+    this.angularFireAuth.authState.pipe(take(1)).subscribe(
+      () => {
+        this.token = token;
+        const data = { token };
+        this.angularFireDB.doc('fcmTokens/all').update(data);
+      });
+  }
+
+  /**
+   * request permission for notification from firebase cloud messaging
+   *
+   * @param userId userId
+   */
+  requestPermission(userId) {
+    this.angularFireMessaging.requestToken.subscribe(
+      (token) => {
+        this.updateToken(userId, token);
+      },
+      (err) => {
+        console.error('Unable to get permission to notify.', err);
+      }
+    );
+  }
+
+  /**
+   * hook method when new notification received in foreground
+   */
+  receiveMessage() {
+    this.angularFireMessaging.messages.subscribe(
+      (payload) => {
+        console.log('new message received. ', payload);
+        this.currentMessage.next(payload);
+      });
+  }
+
+  notifyCoffeeFifthMinutes() {
+    const body = {
+      'notification': {
+        'title': 'Turma do Café',
+        'body': 'O café está pronto!',
+        'icon': 'https://free-images.com/lg/cd8a/coffee_foam_hart_cappuccino.jpg'
+      },
+      'to': '/topics/coffee_alert'
+    };
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `key=${environment.firebase.apiKey}`
+      })
+    };
+    this.http.post('https://fcm.googleapis.com/fcm/send', body, httpOptions)
+    .subscribe(console.log);
+  }
+}
